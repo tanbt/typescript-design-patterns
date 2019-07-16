9688323d2e8a6bf912758289dca7971cc7e084fc
Tan Bui <tan@vaadin.com>
2019-07-16 23:12:17 +0300

    Factory demo

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-import { singletonDemo } from './singleton/singleton-demo';
-singletonDemo();
+import { factoryDemo } from "./factory/factory-demo";
+
+factoryDemo();
+
+//import { singletonDemo } from './singleton/singleton-demo';
+//singletonDemo();
