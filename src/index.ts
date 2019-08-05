7cb88a96704c8f74d0cb2ba6c465d4253396571d
Tan Bui <tan@vaadin.com>
2019-08-05 23:50:54 +0300

    Proxy demo - must test in browser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { flyWeightDemo } from "./flyweight/flyweight-demo";
-flyWeightDemo();
+import { proxyDemo } from "./proxy/proxy-demo";
+proxyDemo();
+// import { flyWeightDemo } from "./flyweight/flyweight-demo";
+// flyWeightDemo();
 
 // import { compositeDemo } from "./composite/composite-demo";
 // compositeDemo();
